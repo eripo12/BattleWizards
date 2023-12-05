27512e3c4b2ec2ba884237828f61921b1b8b32b3
ozwalf <yuyugunfire6@gmail.com>
2023-12-04 19:45:28 -0800

    Playermove damage tweaks
    adjusted player move damage.

--- Assets/Scripts/battleScreen/BattleUI2.cs
+++ Assets/Scripts/battleScreen/BattleUI2.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BattleUI : MonoBehaviour
+public class BattleUI2 : MonoBehaviour
 {
     // Reference for editing the dialogue box
     [SerializeField] BattleDialogue dialogueText;
@@ -53,56 +53,48 @@ public class BattleUI : MonoBehaviour
         UpdateButtons(turnManager.currentTurn);
     }
     void UpdateButtons(TurnManager.Turn currentTurn)
-{
-    bool isPlayerTurn = currentTurn == TurnManager.Turn.Player && !turnManager.isTurnInProgress;
-    Debug.Log("Updating buttons. Player's turn: " + isPlayerTurn);
+    {
+        bool isPlayerTurn = currentTurn == TurnManager.Turn.Player && !turnManager.isTurnInProgress;
+        Debug.Log("Updating buttons. Player's turn: " + isPlayerTurn);
 
-    fireballButton.interactable = isPlayerTurn;
-    waterBlastButton.interactable = isPlayerTurn;
-    lightningBoltButton.interactable = isPlayerTurn;
-    healButton.interactable = isPlayerTurn && (turnManager.turnCount - lastHealTurn >= 2);
-}
+        fireballButton.interactable = isPlayerTurn;
+        waterBlastButton.interactable = isPlayerTurn;
+        lightningBoltButton.interactable = isPlayerTurn;
+        healButton.interactable = isPlayerTurn && (turnManager.turnCount - lastHealTurn >= 2);
+    }
 
     void PlayerFireballAttack()
-    {       
+    {
         turnManager.player.GetComponent<PlayerShooting>().FireballAttack();
-        turnManager.enemy.TakeDamage(5);
+        turnManager.enemy.TakeDamage(15);
         turnManager.EndTurn();
     }
 
     void PlayerWaterBlast()
     {
         Instantiate(waterBlastPrefab, turnManager.player.transform.position, Quaternion.identity);
-        turnManager.enemy.TakeDamage(15);
+        turnManager.enemy.TakeDamage(5);
         turnManager.EndTurn();
     }
 
     void PlayerLightningBolt()
     {
         Instantiate(lightningBoltPrefab, turnManager.player.transform.position, Quaternion.identity);
-        turnManager.enemy.TakeDamage(30);
+        turnManager.enemy.TakeDamage(15);
         turnManager.EndTurn();
     }
 
-   void PlayerHeal()
-{
+    void PlayerHeal()
+    {
 
-    Instantiate(healEffectPrefab, turnManager.player.transform.position, Quaternion.identity);
+        Instantiate(healEffectPrefab, turnManager.player.transform.position, Quaternion.identity);
 
-    
-    turnManager.player.currentHealth = Mathf.Min(turnManager.player.maxHealth, turnManager.player.currentHealth + 20);
-    StartCoroutine(dialogueText.typeText("Player heals " + 20 + " HP."));
-    turnManager.player.healthBar.SetHealth(turnManager.player.currentHealth); // Update the health bar
-    lastHealTurn = turnManager.turnCount;
-    turnManager.EndTurn();
+
+        turnManager.player.currentHealth = Mathf.Min(turnManager.player.maxHealth, turnManager.player.currentHealth + 20);
+        StartCoroutine(dialogueText.typeText("Player heals " + 20 + " HP."));
+        turnManager.player.healthBar.SetHealth(turnManager.player.currentHealth); // Update the health bar
+        lastHealTurn = turnManager.turnCount;
+        turnManager.EndTurn();
+    }
+
 }
-
-}
-
-
-
-
-
-
-
-
